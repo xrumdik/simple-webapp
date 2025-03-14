ad8222ac504eb1089f95a08c7e63466377532574
xrumdik <147482740+xrumdik@users.noreply.github.com>
2025-03-14 10:44:02 +0500

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world, 世界, мир, peace, frieden, paix, paz, pace, paz, vrede, pokój, mír, mier, мір, mir, béke, pace, ειρήνη, barış, rauha, fred, fred, fred, rahu, miers, taika, 和平, 平和, 평화, hòa bình, शांति, سلام, שלום, صلح, امن, สันติภาพ, энх тайван, მშვიდობა, խաղաղություն, бейбітшілік, tinchlik, тынычлык, тыныс, аhар, тынчтык, parahatçylyk, сулҳ, sülh")
+	fmt.Fprintln(w, "Hello, world, 世界, мир, peace, frieden, paix, pace, paz, vrede, pokój, mír, mier, мір, mir, béke, pace, ειρήνη, barış, rauha, fred, fred, fred, rahu, miers, taika, 和平, 平和, 평화, hòa bình, शांति, سلام, שלום, صلح, امن, สันติภาพ, энх тайван, მშვიდობა, խաղաղություն, бейбітшілік, tinchlik, тынычлык, тыныс, аhар, тынчтык, parahatçylyk, сулҳ, sülh")
 }
 
 func main() {
